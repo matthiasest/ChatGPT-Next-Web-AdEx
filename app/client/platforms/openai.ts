b2350697b62d255a5351506fa9176c055a7ef100
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-09-27 17:34:07 +0200

    Update openai.ts

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -6,7 +6,7 @@ import {
 } from "@/app/constant";
 import { useAccessStore, useAppConfig, useChatStore } from "@/app/store";
 
-import { ChatOptions, getHeaders, LLMApi, LLMModel, LLMUsage } from "../api";
+import { ChatOptions, getHeaders, LLMApi, LLMModel, LLMUsage, GPTFunction } from "../api";
 import Locale from "../../locales";
 import {
   EventStreamContentType,
