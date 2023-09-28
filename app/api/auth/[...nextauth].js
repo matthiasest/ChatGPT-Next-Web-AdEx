b78e6f0619afc2048ce8272a0a5df62bbe43f733
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-09-28 17:49:28 +0200

    Create [...nextauth].js

---
+++ app/api/auth/[...nextauth].js
@@ -0,0 +1,17 @@
+console.log('Environment variables:', process.env.AZURE_AD_CLIENT_ID, process.env.AZURE_AD_CLIENT_SECRET, process.env.AZURE_AD_TENANT_ID);
+
+export default NextAuth({
+  providers: [
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+      tenantId: process.env.AZURE_AD_TENANT_ID,
+    }),
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      console.log('NextAuth signIn callback:', user, account, profile);
+      return true;
+    },
+  },
+});
