508108ab603d1b9c6a442ce761242b80b0630db4
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-09-28 18:05:14 +0200

    Update [...nextauth].ts

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
 console.log('Environment variables:', process.env.AZURE_AD_CLIENT_ID, process.env.AZURE_AD_CLIENT_SECRET, process.env.AZURE_AD_TENANT_ID);
 
+
+import NextAuth from 'next-auth'
+import AzureADProvider from 'next-auth/providers/azure-ad'
+
 export default NextAuth({
   providers: [
     AzureADProvider({
