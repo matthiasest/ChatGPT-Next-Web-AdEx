886d3c0210fba8276a49a485cd318a96de5b9a8d
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-09-28 16:23:23 +0200

    Update [...nextauth].js

--- app/api/auth/[...nextauth].js
+++ app/api/auth/[...nextauth].js
@@ -1,9 +1,15 @@
+import NextAuth from "next-auth";
 import AzureADProvider from "next-auth/providers/azure-ad";
 
-providers: [
-  AzureADProvider({
-    clientId: process.env.AZURE_AD_CLIENT_ID,
-    clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
-    tenantId: process.env.AZURE_AD_TENANT_ID,
-  }),
-]
+export default NextAuth({
+  providers: [
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+      tenantId: process.env.AZURE_AD_TENANT_ID
+      // You may include additional Azure AD options here
+    }),
+    // ...you can add more providers here if needed in the future
+  ],
+  // Additional NextAuth configurations go here (like session handling, JWT, etc.)
+});
