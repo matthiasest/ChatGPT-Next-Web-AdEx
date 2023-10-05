08534aaefaa38b668c22e9ad4a0ef68797c01d72
matthiasest <146088777+matthiasest@users.noreply.github.com>
2023-10-05 11:40:36 +0200

    Create auth.ts

---
+++ app/auth.ts
@@ -0,0 +1,54 @@
+import type { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from "next"
+import type { NextAuthOptions as NextAuthConfig } from "next-auth"
+import { getServerSession } from "next-auth"
+
+
+import AzureADProvider from "next-auth/providers/azure-ad"
+
+
+// Read more at: https://next-auth.js.org/getting-started/typescript#module-augmentation
+declare module "next-auth/jwt" {
+  interface JWT {
+    /** The user's role. */
+    userRole?: "admin"
+  }
+}
+
+export const config = {
+  providers: [
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+      tenantId: process.env.AZURE_AD_TENANT_ID,
+    }), 
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token }) {
+      token.userRole = "admin"
+      return token
+    },
+  },
+} satisfies NextAuthConfig
+
+// Helper function to get session without passing config every time
+// https://next-auth.js.org/configuration/nextjs#getserversession
+export function auth(...args: [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]] | [NextApiRequest, NextApiResponse] | []) {
+  return getServerSession(...args, config)
+}
+
+// We recommend doing your own environment variable validation
+declare global {
+  namespace NodeJS {
+    export interface ProcessEnv {
+      NEXTAUTH_SECRET: string
+
+    
+      AZURE_AD_CLIENT_ID: string
+      AZURE_AD_CLIENT_SECRET: string
+      AZURE_AD_TENANT_ID: string
+       
+     
+    }
+  }
+}
