fd3b9ce9f988f13a6759cdb221197dea966d8fd3
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-10-07 16:04:26 +0200

    Update markdown.tsx

--- app/components/markdown.tsx
+++ app/components/markdown.tsx
@@ -181,12 +181,12 @@ console.log("     setGptFunctionObj: ",    { ...json }, gptFunctionObj);
           </div>
         </div>
       )}
-      {jsonObj && 'choices' in jsonObj && jsonObj.choices && jsonObj.choices[0] && jsonObj.choices[0].message && jsonObj.choices[0].message.function_call && jsonObj.choices[0].message.function_call.name && (
+      {jsonObj && 'choices' in jsonObj && jsonObj.choices && jsonObj.choices[0] && jsonObj.choices[0].message && jsonObj.choices[0].message.function_call && jsonObj.choices[0].message.function_call.name && jsonObj.choices[0].message.function_call.arguments && (
         <div style={{ border: "1px solid blue", padding: "1em", marginBottom: "1em" }}>
           <h2>Function Call: {jsonObj.choices[0].message.function_call.name}</h2>
           <div>
             <h3>Arguments:</h3>
-                {Object.entries(JSON.parse(gptChatCompletionObj.choices[0].message.function_call.arguments as string)).map(([key, value]) => (
+                {Object.entries(JSON.parse(jsonObj.choices[0].message.function_call.arguments as string)).map(([key, value]) => (
                 <div style={{ border: "1px solid green", padding: "1em", marginBottom: "1em" }} key={key}>
                   {key}: {typeof value === 'string' || typeof value === 'number' ? value : 'Unknown value type'}
                 </div>
