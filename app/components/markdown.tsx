30247f59a6a4eaec507a9affa2594b3b97b99862
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-10-07 15:52:00 +0200

    Update markdown.tsx

--- app/components/markdown.tsx
+++ app/components/markdown.tsx
@@ -186,7 +186,11 @@ console.log("     setGptFunctionObj: ",    { ...json }, gptFunctionObj);
           <h2>Function Call: {jsonObj.choices[0].message.function_call.name}</h2>
           <div>
             <h3>Arguments:</h3>
-
+                {Object.entries(JSON.parse(gptChatCompletionObj.choices[0].message.function_call.arguments as string)).map(([key, value]) => (
+                <div style={{ border: "1px solid green", padding: "1em", marginBottom: "1em" }} key={key}>
+                  {key}: {typeof value === 'string' || typeof value === 'number' ? value : 'Unknown value type'}
+                </div>
+            ))}
           </div>
         </div>
       )}
