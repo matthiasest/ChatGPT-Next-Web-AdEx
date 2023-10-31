4dabd729e5c3b58b8a2b14d6c7be6b1eb2972763
matthiasest <146088777+matthiasest@users.noreply.github.com>
2023-10-31 15:30:12 +0100

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -17,6 +17,13 @@ import { ErrorBoundary } from "./error";
 
 import { getISOLang, getLang } from "../locales";
 
+import type { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from "next"
+import type { NextAuthOptions as NextAuthConfig } from "next-auth"
+import { getServerSession } from "next-auth"
+
+
+import AzureADProvider from "next-auth/providers/azure-ad"
+
 import {
   HashRouter as Router,
   Routes,
@@ -38,6 +45,75 @@ export function Loading(props: { noLogo?: boolean }) {
     </div>
   );
 }
+
+
+// Read more at: https://next-auth.js.org/getting-started/typescript#module-augmentation
+declare module "next-auth/jwt" {
+  interface JWT {
+    /** The user's role. */
+    userRole?: "admin"
+  }
+}
+
+export const config = {
+  providers: [
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+      tenantId: process.env.AZURE_AD_TENANT_ID,
+    }), 
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token }) {
+      token.userRole = "admin"
+      return token
+    },
+  },
+} satisfies NextAuthConfig
+
+// Helper function to get session without passing config every time
+// https://next-auth.js.org/configuration/nextjs#getserversession
+export function auth(...args: [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]] | [NextApiRequest, NextApiResponse] | []) {
+  return getServerSession(...args, config)
+}
+
+// We recommend doing your own environment variable validation
+declare global {
+  namespace NodeJS {
+    export interface ProcessEnv {
+      NEXTAUTH_SECRET: string
+
+    
+      AZURE_AD_CLIENT_ID: string
+      AZURE_AD_CLIENT_SECRET: string
+      AZURE_AD_TENANT_ID: string
+       
+     
+    }
+  }
+}
+
+import NextAuth from "next-auth/next"
+
+// This is an example of how to read a JSON Web Token from an API route
+import { getToken } from "next-auth/jwt"
+
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // If you don't have the NEXTAUTH_SECRET environment variable set,
+  // you will have to pass your secret as `secret` to `getToken`
+  const token = await getToken({ req })
+  res.send(JSON.stringify(token, null, 2))
+}
+
+export { handler as GET, handler as POST }
+
+
 
 const Settings = dynamic(async () => (await import("./settings")).Settings, {
   loading: () => <Loading noLogo />,
@@ -136,6 +212,11 @@ function Screen() {
   }, []);
 
   console.log("Screen: isAuth - ", isAuth);
+
+  const handler = NextAuth(config)
+  
+
+
 
   return (
     <div
