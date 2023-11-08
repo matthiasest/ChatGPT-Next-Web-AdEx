3cc185060643c315f6fbdc8fc57b73faecbc6858
matthiasest <146088777+matthiasest@users.noreply.github.com>
2023-11-08 09:28:42 +0100

    Update openai.ts

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -53,11 +53,11 @@ export class ChatGPTApi implements LLMApi {
       role: v.role,
       content: [
           { type: "text", text: v.content },
-          {
+          /*{
             type: "image_url",
             image_url:
               "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
-          },
+          },*/
           ],
       }));
     }
