c513e54bcbab74aa314aad39f8172fcdf1ad086f
matthiasest <146088777+matthiasest@users.noreply.github.com>
2023-11-08 09:45:06 +0100

    Update openai.ts

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -50,8 +50,15 @@ export class ChatGPTApi implements LLMApi {
 
   async chat(options: ChatOptions) {
     const messages = options.messages.map((v) => ({
-      role: v.role,
-      content: v.content,  //Hier
+      role: "user",
+        content: [
+          { type: "text", text: "What’s in this image?" },
+          {
+            type: "image_url",
+            image_url:
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+          },
+        ],
     }));
 
     const modelConfig = {
